b20fe8d16c9b9e0d312cb6d33849eb0a70b9daa4
fhelipex <138606213+fhelipex@users.noreply.github.com>
2023-07-06 12:30:30 +0800

    Add files via upload

---
+++ CofeeShop/src/main/java/com/mycompany/cofeeshop/radCOFFEE.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cofeeshop;
+
+/**
+ *
+ * @author philip
+ */
+public class radCOFFEE{
+     
+
+if(radCOFFEE.isSelected())
+            radCAP.setSelected(false);
+            radTEA.setSelected(false);
+    
+}
